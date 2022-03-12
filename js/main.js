b42e8ab0be6f4512b71fb1c7bad5667f70cd3dff
Anastasiia Rakhmatova <rakhmatova-93@mail.ru>
2022-03-13 00:57:00 +0300

    Check main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 
-// function checkStringLength(string, maxLength) {
-//   return string.length <= maxLength;
-// }
-
-// checkStringLength('Привет', 10);
-
 const NOTES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -14,7 +8,7 @@ const NOTES = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 const NAMES = ['Анастасия', 'Кирилл', 'Надежда', 'Ирина', 'Сергей'];
-const RANDOM_POSTS_COUNT = 25;
+const POSTS_COUNT = 25;
 
 function getRandomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -22,43 +16,56 @@ function getRandomInteger(min, max) {
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-const createPost = () => ({
-  id: getRandomInteger(1, 25),
-  url: `photos/${getRandomInteger(1, 25)}.jpg`,
-  description: 'Как же прекрасно это время года!',
-  likes: getRandomInteger(15, 200),
-  comments: [
-    {
-      id: 135,
-      avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-      message: getRandomArrayElement(NOTES),
-      name: getRandomArrayElement(NAMES),
-    },
-  ],
-});
 
-const randomPosts = Array.from({length: RANDOM_POSTS_COUNT}, createPost);
+const checkId = (array) => {
+  let number = getRandomInteger (1, 100);
+  for(let i = 0; i <= array.length - 1; i++) {
 
-// Определение кол-ва комментариев
+    for (let j = 0; j <= array[i].comments.length - 1; j++) {
+      const innerArray = array[i].comments;
 
-// const createComment = () => ({
-//   id: 135,
-//   avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-//   message: getRandomArrayElement(NOTES),
-//   name: getRandomArrayElement(NAMES),
-// });
+      // В этом цикле перебираем здесь каждый объект в массиве comments
+      // Айдишники собираем в новый массив ids, есди такого еще нет
+      // В самый первый элемент массива записываем первое значение number
+      // Если это не первый цикл первого цикла и значение в новом массиве не равно number, то добавляем в этот массив новый элемент и number оставляем неизменяемым
+      // А если не подходит условию, то вызываем снова функцию getRandomInteger(1,100) для number
 
-// const createCommentsNumber = function () {
-//   return Array.from({length: getRandomInteger(1, 5)}, createComment);
-// };
+      const ids = [];
 
-// const createPost = () => ({
-//   id: getRandomInteger(1, 25),
-//   url: `photos/${getRandomInteger(1, 25)}.jpg`,
-//   description: 'Как же прекрасно это время года!',
-//   likes: getRandomInteger(15, 200),
-//   comments: createCommentsNumber(),
-// });
+      if (i === 0 && j === 0) {
+        ids[0] = number;
+      }
 
-// const randomPosts = Array.from({length: RANDOM_POSTS_COUNT}, createPost);
-// console.log(randomPosts);
+      const isExistingId = ids.some((value) => value === number);
+
+      if(!(i === 0 && j === 0) && !isExistingId) {
+        number = getRandomInteger (1, 100);
+      } else {
+        ids.push(innerArray[j].id);
+      }
+    }
+  }
+  return number;
+};
+
+const posts = [];
+
+for (let i = 1; i <= POSTS_COUNT; i++) {
+  const post = {
+    id: i,
+    url: `photos/${i}.jpg`,
+    description: 'Как же прекрасно это время года!',
+    likes: getRandomInteger(15, 200),
+    comments: [
+      {
+        id: checkId(posts),
+        avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+        message: getRandomArrayElement(NOTES),
+        name: getRandomArrayElement(NAMES),
+      },
+    ],
+  };
+  posts.push(post);
+}
+
+console.log(posts);
