943b2288e0a2eae2ae14e2c5f4fdbb0d21faafc1
Keks <keks@htmlacademy.ru>
2022-03-22 11:22:10 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/full-size.js
+++ 6/js/full-size.js
@@ -1,18 +1,26 @@
 import {createPictures} from './data.js';
-const newPictures = createPictures();
+import {getRandomArrayElement} from './util.js';
 
 const bigPictureBlock = document.querySelector('.big-picture');
-bigPictureBlock.classList.remove('hidden');
-
 const bigPictureImg = bigPictureBlock.querySelector('.big-picture__img');
 const likesCount = bigPictureBlock.querySelector('.likes-count');
 const commentsCount = bigPictureBlock.querySelector('.comments-count');
 const socialComments = bigPictureBlock.querySelector('.social__comments');
 const photoDescription = bigPictureBlock.querySelector('.social__caption');
 
-const createSocialComments = (pictures) => {
+const commentsCountBlock = bigPictureBlock.querySelector('.social__comment-count');
+const showMore = bigPictureBlock.querySelector('.comments-loader');
 
-  pictures.forEach((picture) => {
+const fullSizePicture = getRandomArrayElement(createPictures());
+
+bigPictureBlock.classList.remove('hidden');
+document.body.classList.add('modal-open');
+commentsCountBlock.classList.add('hidden');
+showMore.classList.add('hidden');
+
+
+const createSocialComments = (picture) => {
+  picture.comments.forEach((comment) => {
     const socialComment = document.createElement('li');
     const socialImg = document.createElement('img');
     const socialText = document.createElement('p');
@@ -21,30 +29,28 @@ const createSocialComments = (pictures) => {
     socialImg.classList.add('social__picture');
     socialText.classList.add('social__text');
 
-    picture.comments.forEach((comment) => {
-      socialImg.src = comment.avatar;
-      socialImg.alt = comment.name;
-      socialImg.width = 35;
-      socialImg.height = 35;
-      socialText.textContent = comment.message;
+    socialImg.src = comment.avatar;
+    socialImg.alt = comment.name;
+    socialImg.width = 35;
+    socialImg.height = 35;
+    socialText.textContent = comment.message;
 
-      socialComment.append(socialImg);
-      socialComment.append(socialText);
-    });
-
+    socialComment.append(socialImg);
+    socialComment.append(socialText);
     socialComments.append(socialComment);
   });
-
   return socialComments;
 };
 
-const drawBigPictureBlock = (pictures) => {
-  bigPictureImg.src = pictures.url;
-  likesCount.textContent = pictures.likes;
-  commentsCount.textContent = pictures.comments;
+const drawBigPictureBlock = (picture) => {
+  bigPictureImg.querySelector('img').src = picture.url;
+  likesCount.textContent = picture.likes;
+  commentsCount.textContent = picture.comments.length;
   socialComments.innerHtml = '';
-  socialComments.innerHtml = createSocialComments(pictures);
-  photoDescription.textContent = pictures.description;
+  socialComments.innerHtml = createSocialComments(picture);
+  photoDescription.textContent = picture.description;
 };
 
-drawBigPictureBlock(newPictures);
+drawBigPictureBlock(fullSizePicture);
+
+export {bigPictureBlock};
